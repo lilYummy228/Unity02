0083d5bac9cffc4a415438b45a509317741f5aa4
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-01-21 14:13:54 +0500

    123

--- Assets/Scripts/2D Platformer/ScoreCounter.cs
+++ Assets/Scripts/2D Platformer/ScoreCounter.cs
@@ -6,14 +6,15 @@ public class ScoreCounter : MonoBehaviour
     [SerializeField] private TMP_Text _scoreCounter;
 
     private int _scoreCount;
+
+    public void AddScore()
+    {
+        _scoreCount++;
+    }
        
     private void Update()
     {
         _scoreCounter.text = _scoreCount.ToString();
     }
 
-    public void AddScore()
-    {
-        _scoreCount++;
-    }
 }
